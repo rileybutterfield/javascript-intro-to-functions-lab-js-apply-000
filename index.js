ad848b5d09a4cdbb4c2189184d24db494fe0348a
Riley Butterfield <rileybutterfield1@gmail.com>
2020-04-16 20:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ function whisper(string){
 function logShout(string){
   console.log(string.toUpperCase());
 }
-fu
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+function sayHiToGrandma(string){
+  if (string === ) 
+}
